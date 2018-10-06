a56e6bf37302761e2488a6e3e12cb45aa9aee49d
Martynas Mickevičius <self@2m.lt>
2018-10-06 17:16:27 -0500

    Initial commit

---
+++ functions/jabba.fish
@@ -0,0 +1,8 @@
+function fish
+    set fd3 (mktemp /tmp/jabba-fd3.XXXXXX)
+    env JABBA_SHELL_INTEGRATION=ON jabba $argv 3> $fd3
+    set exit_code \$status
+    eval (cat $fd3 | sed "s/^export/set -xg/g" | sed "s/^unset/set -e/g" | tr '=' ' ' | sed "s/:/\\" \\"/g" | tr '\\n' ';')
+    rm -f $fd3
+    return $exit_code
+end
